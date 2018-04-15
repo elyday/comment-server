6c8572197421d66563301d08040bea87457caae8
larsr <me@elyday.net>
2018-04-15 22:49:20 +0200

    #1 Controller implement finished

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Blog;
+use App\BlogArticle;
 use App\Helper\FormatHelper;
 
 /**
@@ -16,10 +17,25 @@ class BlogController extends Controller
         return FormatHelper::formatData(Blog::all());
     }
 
-    public function getBlog($hash)
+    public function getBlog($blogHash)
     {
         $blog = new Blog();
-        $return = $blog->where("hash", $hash)->first();
+        $return = $blog->where("hash", $blogHash)->first();
         return $return != null ? FormatHelper::formatData($return) : FormatHelper::formatData(array(), FALSE);
+    }
+
+    public function getBlogWithArticles($blogHash)
+    {
+        $blog = new Blog();
+        $blogResult = $blog->where("hash", $blogHash)->first();
+
+        if ($blogResult != null) {
+            $blogArticle = new BlogArticle();
+            $return = $blogArticle->where("blogId", $blogResult["id"])->get();
+
+            return $return != null ? FormatHelper::formatData($return) : FormatHelper::formatData(array(), FALSE);
+        } else {
+            return FormatHelper::formatData(array(), false);
+        }
     }
 }
